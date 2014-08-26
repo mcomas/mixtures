21facc809cdcab60455da4793dd2655eab81e43b
mcomas <marc@Marcs-Mac-mini.local>
2014-08-27 00:16:55 +0200

    - Es fan les modificacions de la versió amb data 22 d'agost. - Es fa dependre la generació de dades del càlcul dels paràmetres de la Dirichlet.

--- compositional_mixtures/R/parameters_dirichlet_mixture.R
+++ compositional_mixtures/R/parameters_dirichlet_mixture.R
@@ -13,15 +13,26 @@ load('data/dirichlet_mixture_parameters.RData')
 
 sink('tex/pars_dirichlet_mixture.tex')
 cat('\\[\n')
-cat('\\begin{array}{c@{\\hskip 0.1in}c@{\\hskip 0.1in}c }\n')
+cat('\\begin{array}{l@{\\hskip 0.1in}l }\n')
 #cat('\\hline ')
-cat(sprintf("\\pi_1 = %3.2f, & \\pi_2 = %3.2f, & \\pi_3 = %3.2f, \\\\ & &\\\\ \n",
-            pi[1], pi[2], pi[3]))
 
-cat(sprintf("\\alpha_1 = \\left(%s\\right), & \\alpha_2 = \\left(%s\\right), & \\alpha_3 = \\left(%s\\right). \\\\ & &\\\\ \n",
-            paste(round(alpha[[1]],1), collapse = ', '),
-            paste(round(alpha[[2]],1), collapse = ', '),
-            paste(round(alpha[[3]],1), collapse = ', ')))
+cat(sprintf("\\pi_1 = %3.2f, & \\alpha_1 = \\left(%s\\right), \\\\ & \\\\ \n",
+            pi[1], paste(round(alpha[[1]],1), collapse = ', ')))
+
+cat(sprintf("\\pi_2 = %3.2f, & \\alpha_2 = \\left(%s\\right), \\\\ & \\\\ \n",
+            pi[2], paste(round(alpha[[2]],1), collapse = ', ')))
+
+cat(sprintf("\\pi_3 = %3.2f \\text{ and }& \\alpha_3 = \\left(%s\\right). \\\\ & \\\\ \n",
+            pi[3], paste(round(alpha[[3]],1), collapse = ', ')))
+
+
+# cat(sprintf("\\pi_1 = %3.2f, & \\pi_2 = %3.2f, & \\pi_3 = %3.2f, \\\\ & &\\\\ \n",
+#             pi[1], pi[2], pi[3]))
+# 
+# cat(sprintf("\\alpha_1 = \\left(%s\\right), & \\alpha_2 = \\left(%s\\right), & \\alpha_3 = \\left(%s\\right). \\\\ & &\\\\ \n",
+#             paste(round(alpha[[1]],1), collapse = ', '),
+#             paste(round(alpha[[2]],1), collapse = ', '),
+#             paste(round(alpha[[3]],1), collapse = ', ')))
 
 #cat('\\hline')
 cat('\\end{array}\n')
