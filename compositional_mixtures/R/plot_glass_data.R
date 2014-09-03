203c55b3fd6263863fd73dc4bfc2e7cf18a1adf9
marc_comas <marc.comas@gmail.com>
2014-09-03 21:13:38 +0200

    s'afageixen modificacions a figures i es continua amb l'article combining mixtures

--- compositional_mixtures/R/plot_glass_data.R
+++ compositional_mixtures/R/plot_glass_data.R
@@ -16,36 +16,39 @@ df.win = data.frame(
           Si=c(0.8, 0.8, 1.0 ),
           Al=c(0.0, 0.2, 0.0),
           glasses=c(NA,NA,NA))
-p0 = ggtern(data=df, aes(Ca, Al, Si, col=glasses))+geom_point(size=1.1) + 
+p0 = ggtern(data=df, aes(Ca, Al, Si, shape=glasses))+
   theme_rgbw() +
-  scale_color_manual(values=c("black", "#CC6666", "#66CC99"), 
-                   breaks=c("Con", "Head", "Veh"),
-                   labels=c("container", "headlamp", "vehicle window"))
+  scale_shape_manual(values=c(1, 2, 0),
+                     breaks=c("Con", "Head", "Veh"),
+                     labels=c("container", "headlamp", "vehicle window"))
+#   scale_color_manual(values=c("black", "#CC6666", "#66CC99"), 
+#                    breaks=c("Con", "Head", "Veh"),
+#                    labels=c("container", "headlamp", "vehicle window"))
 
-pa = p0 + geom_polygon(data=df.win, color="red",alpha=0.25,size=1,linetype=1) +
+pa = p0 + geom_polygon(data=df.win, color="red",alpha=0.25,size=1,linetype=1) + geom_point(size=1.2) +
   scale_T_continuous(breaks=seq(0.2,0.8,0.2),minor_breaks=seq(0,1,0.05)) + 
   scale_L_continuous(breaks=seq(0.2,0.8,0.2),minor_breaks=seq(0,1,0.05)) + 
   scale_R_continuous(breaks=seq(0.2,0.8,0.2),minor_breaks=seq(0,1,0.05)) +
-  guides(col=FALSE)
+  guides(shape=FALSE)
 
-pb = p0 + geom_point(size=3) + coord_tern(Tlim=c(0,.2), Llim=c(0.0,0.2), Rlim=c(0.8,1.0)) +
+pb = p0 + geom_point(size=4) + coord_tern(Tlim=c(0,.2), Llim=c(0.0,0.2), Rlim=c(0.8,1.0)) +
   scale_T_continuous(breaks=seq(0.05,0.15,0.05),minor_breaks=seq(0,1,0.05)) + 
   scale_L_continuous(breaks=seq(0.05,0.15,0.05),minor_breaks=seq(0,1,0.05)) + 
   scale_R_continuous(breaks=seq(0.85,0.95,0.05),minor_breaks=seq(0,1,0.05)) + 
   ggtitle("Forensic Glass data set") + 
   theme(plot.title = element_text(lineheight=.8, face="bold"),
         legend.title = element_blank(),
-        legend.text = element_text(colour="black", size = 12),
+        legend.text = element_text(colour="black", size = 14),
         legend.position="bottom",
         legend.key = element_blank())
 
 pa = pa+theme_classic() + scale_color_hue()
 pb = pb+theme_classic() + scale_color_hue()
 
-pdf(file='figures//main_df.pdf', width=9.5, height=4.4, pointsize=10)
+pdf(file='figures//main_df.pdf', width=11.72, height=6.4)
 grid.newpage()
-vpb_ <- viewport(width = 0.46, height = 0.46, x = 0.15, y = 0.74)  # the larger map
-vpa_ <- viewport(width = 1, height = 1, x = 0.5, y = 0.49 )  # the inset in upper right
+vpb_ <- viewport(width = 0.46, height = 0.46, x = 0.2, y = 0.7)  # the larger map
+vpa_ <- viewport(width = 1, height = 1, x = 0.6, y = 0.49 )  # the inset in upper right
 print(pb, vp = vpa_)
 print(pa, vp = vpb_)
 dev.off()
