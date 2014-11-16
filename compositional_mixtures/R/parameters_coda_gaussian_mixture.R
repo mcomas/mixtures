d4392ca4fa159bc7171184df0a35c8336972e8c4
mcomas <marc@Marcs-Mac-mini.local>
2014-11-16 19:31:53 +0100

    * Es canvien els paràmetres de les distribucions normals a la matriu de covariàncies. * Es canvia el format de les estimacions utilitzant \hat i negreta * Es modifica la referència d'Aichison

--- compositional_mixtures/R/parameters_coda_gaussian_mixture.R
+++ compositional_mixtures/R/parameters_coda_gaussian_mixture.R
@@ -20,6 +20,8 @@ P  = round(mm@bestResult@parameters@proportions, 2)
 
 M  = round(mm@bestResult@parameters@mean, 2)
 
+S = llply(mm@bestResult@parameters@variance, round, 4)
+
 R = llply(llply(mm@bestResult@parameters@variance, cov2cor), round, 2)
 
 V = llply(llply(mm@bestResult@parameters@variance, function(v) matrix(diag(v), ncol=1)), round, 3)
@@ -30,33 +32,30 @@ sink('tex/pars_coda_gaussian_mixture.tex')
 cat('\\[\n')
 cat('\\begin{array}{l@{\\hskip 0.1in}l@{\\hskip 0.1in}c }\n')
 
-cat(sprintf("\\pi_1 = %s, & \\mu_1 = \\left(%s\\right), & \\Sigma_1 = \\left(\n%s\\right) \\cdot \\left(\n%s\\right), \\\\ & &\\\\ \n",
+cat(sprintf("\\hat{\\pi}_1 = %s, & \\hat{\\m\\mu}_1 = \\left(%s\\right), & \\hat{\\m\\Sigma}_1 = \\left(\n%s\\right), \\\\ & &\\\\ \n",
             paste(P[1], collapse = ','),
             paste(M[1,], collapse = ','),
-            str_replace_all(latexTabular(R[[1]], headings = NULL, helvetica = FALSE), 'tabular', 'array'),
-            str_replace_all(latexTabular(V[[1]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
+            str_replace_all(latexTabular(S[[1]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
 
 cat('\\end{array}\n')
 cat('\\]\n')
 cat('\\[\n')
 cat('\\begin{array}{l@{\\hskip 0.1in}l@{\\hskip 0.1in}l }\n')
 
-cat(sprintf("\\pi_2 = %s, & \\mu_2 = \\left(%s\\right), & \\Sigma_2 = \\left(\n%s\\right) \\cdot \\left(\n%s\\right), \\\\ & &\\\\ \n",
+cat(sprintf("\\hat{\\pi}_2 = %s, & \\hat{\\m\\mu}_2 = \\left(%s\\right), & \\hat{\\m\\Sigma}_2 = \\left(\n%s\\right), \\\\ & &\\\\ \n",
             paste(P[2], collapse = ','),
             paste(M[2,], collapse = ','),
-            str_replace_all(latexTabular(R[[2]], headings = NULL, helvetica = FALSE), 'tabular', 'array'),
-            str_replace_all(latexTabular(V[[2]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
+            str_replace_all(latexTabular(S[[2]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
 
 cat('\\end{array}\n')
 cat('\\]\n')
 cat('\\[\n')
 cat('\\begin{array}{l@{\\hskip 0.1in}l@{\\hskip 0.1in}l }\n')
 
-cat(sprintf("\\pi_3 = %s, & \\mu_3 = \\left(%s\\right) \\text{ and } & \\Sigma_3 = \\left(\n%s\\right) \\cdot \\left(\n%s\\right). \\\\ & &\\\\ \n",
+cat(sprintf("\\hat{\\pi}_3 = %s, & \\hat{\\m\\mu}_3 = \\left(%s\\right) \\text{ and } & \\hat{\\m\\Sigma}_3 = \\left(\n%s\\right). \\\\ & &\\\\ \n",
             paste(P[3], collapse = ','),
             paste(M[3,], collapse = ','),
-            str_replace_all(latexTabular(R[[3]], headings = NULL, helvetica = FALSE), 'tabular', 'array'),
-            str_replace_all(latexTabular(V[[3]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
+            str_replace_all(latexTabular(S[[3]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
 
 # cat(sprintf("\\pi_1 = %s, & \\pi_2 = %s, & \\pi_3 = %s, \\\\ & &\\\\ \n",
 #             paste(P[1], collapse = ','),
--- compositional_mixtures/R/parameters_component_elimination.R
+++ compositional_mixtures/R/parameters_component_elimination.R
@@ -24,6 +24,7 @@ M1  = round(mm1@bestResult@parameters@mean, 2)
 M2  = round(mm2@bestResult@parameters@mean, 2)
 M3  = round(mm3@bestResult@parameters@mean, 2)
 
+S1 = llply(mm1@bestResult@parameters@variance, round, 2)
 
 R1 = llply(llply(mm1@bestResult@parameters@variance, cov2cor), round, 2)
 V1 = llply(llply(mm1@bestResult@parameters@variance, function(v) matrix(diag(v), ncol=1)), round, 3)
@@ -39,33 +40,30 @@ cat('\\[\n')
 cat('\\begin{array}{l@{\\hskip 0.1in}l@{\\hskip 0.1in}l }\n')
 #cat('\\hline ')
 
-cat(sprintf("\\pi_1 = %s, & \\mu_1 = \\left(%s\\right), & \\Sigma_1 = \\left(\n%s\\right) \\cdot \\left(\n%s\\right), \\\\ & &\\\\ \n",
+cat(sprintf("\\hat{\\pi}_1 = %s, & \\hat{\\m\\mu}_1 = \\left(%s\\right), & \\hat{\\m\\Sigma}_1 = \\left(\n%s\\right), \\\\ & &\\\\ \n",
             paste(P1[1], collapse = ','),
             paste(M1[1,], collapse = ','),
-            str_replace_all(latexTabular(R1[[1]], headings = NULL, helvetica = FALSE), 'tabular', 'array'),
-            str_replace_all(latexTabular(V1[[1]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
+            str_replace_all(latexTabular(S1[[1]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
 
 cat('\\end{array}\n')
 cat('\\]\n')
 cat('\\[\n')
 cat('\\begin{array}{l@{\\hskip 0.1in}l@{\\hskip 0.1in}l }\n')
 
-cat(sprintf("\\pi_2 = %s, & \\mu_2 = \\left(%s\\right), & \\Sigma_2 = \\left(\n%s\\right) \\cdot \\left(\n%s\\right), \\\\ & &\\\\ \n",
+cat(sprintf("\\hat{\\pi}_2 = %s, & \\hat{\\m\\mu}_2 = \\left(%s\\right), & \\hat{\\m\\Sigma}_2 = \\left(\n%s\\right), \\\\ & &\\\\ \n",
             paste(P1[2], collapse = ','),
             paste(M1[2,], collapse = ','),
-            str_replace_all(latexTabular(R1[[2]], headings = NULL, helvetica = FALSE), 'tabular', 'array'),
-            str_replace_all(latexTabular(V1[[2]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
+            str_replace_all(latexTabular(S1[[2]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
 
 cat('\\end{array}\n')
 cat('\\]\n')
 cat('\\[\n')
 cat('\\begin{array}{l@{\\hskip 0.1in}l@{\\hskip 0.1in}l }\n')
 
-cat(sprintf("\\pi_3 = %s, & \\mu_3 = \\left(%s\\right) \\text{ and }& \\Sigma_3 = \\left(\n%s\\right) \\cdot \\left(\n%s\\right). \\\\ & &\\\\ \n",
+cat(sprintf("\\hat{\\pi}_3 = %s, & \\hat{\\m\\mu}_3 = \\left(%s\\right) \\text{ and }& \\hat{\\m\\Sigma}_3 = \\left(\n%s\\right). \\\\ & &\\\\ \n",
             paste(P1[3], collapse = ','),
             paste(M1[3,], collapse = ','),
-            str_replace_all(latexTabular(R1[[3]], headings = NULL, helvetica = FALSE), 'tabular', 'array'),
-            str_replace_all(latexTabular(V1[[3]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
+            str_replace_all(latexTabular(S1[[3]], headings = NULL, helvetica = FALSE), 'tabular', 'array')))
 
 # cat(sprintf("\\pi_1 = %s, & \\pi_2 = %s, & \\pi_3 = %s, \\\\ & &\\\\ \n",
 #             paste(P1[1], collapse = ','),
